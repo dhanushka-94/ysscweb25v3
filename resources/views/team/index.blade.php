a85bf2fb3492a3553483c89e83a86e764e190b08
Your Name <you@example.com>
2025-10-27 21:08:32 +0530

    Remove duplicate breadcrumbs from Our Team page

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -12,31 +12,6 @@
     </div>
 </section>
 
-<!-- Breadcrumbs -->
-<div class="bg-gray-50 py-4">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <nav class="flex" aria-label="Breadcrumb">
-            <ol class="flex items-center space-x-2">
-                @foreach($breadcrumbs as $breadcrumb)
-                    <li class="flex items-center">
-                        @if($breadcrumb['url'])
-                            <a href="{{ $breadcrumb['url'] }}" class="text-yellow-600 hover:text-yellow-700 font-medium">
-                                {{ $breadcrumb['title'] }}
-                            </a>
-                        @else
-                            <span class="text-gray-500 font-medium">{{ $breadcrumb['title'] }}</span>
-                        @endif
-                        @if(!$loop->last)
-                            <svg class="w-4 h-4 mx-2 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                            </svg>
-                        @endif
-                    </li>
-                @endforeach
-            </ol>
-        </nav>
-    </div>
-</div>
 
 <!-- Team Categories -->
 <div class="py-16 bg-white">
